ca0b4201f644bb26351eb45d8bce9a514dccbb0c
Alexandra <ss_sergeeva167@mail.ru>
2022-11-06 20:55:45 +0000

    changes

--- src/viking.js
+++ src/viking.js
@@ -1,13 +1,72 @@
 // Soldier
 class Soldier {
-    constructor() {}
+    constructor (health, strength) {
+        this.health = health,
+        this.strength = strength
+    }
+
+    attak(){
+        return this.strength;
+    }
+
+
+reciveDamage(damage){
+    this.health -= damage;
+ }
 }
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name,health,strength){
+        super(health,strength)
+        this.name = name
+    }
+
+    reciveDamage(demage){
+        this.health -= damage;  
+
+    if(this.health > 0) return '${this.name} has received ${damage} points of damage'
+    if(this.health <= 0) return '${this.name} has died in act of combat'
+
+}
+
+    battleCry(){
+       return 'Odin Owns You All!'
+}
 
 // Saxon
-class Saxon {}
+
+class Saxon extends Soldier{
+    reciveDamage(damage){
+        this.health -= damage;
+
+        if(this.health > 0) return 'A Saxon has received ${damage} points of damage'
+        if(this.health <= 0) return 'A Saxon has died in act of combat' 
+    }
+
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingsArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addVikings (newViking){
+        this.vikingsArmy.push(newViking)
+    }
+
+    addSaxon(newSaxon) {
+        this.saxonArmy.push(newSaxom)
+    }
+
+    getRandom (army){
+        return Math.floor(Math.random() * army.length)
+    }
+
+    vikingsAttak(){
+        const v
+    }
+}
+
